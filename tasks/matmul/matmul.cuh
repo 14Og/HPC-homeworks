f0c9cc8a06635dc5f6c574ae39a8e2e98aaa35c2
14Og <kolya.kiz@yandex.ru>
2026-06-26 11:50:01 +0000

    (impl) autosync: 11:50:01

--- tasks/matmul/matmul.cuh
+++ tasks/matmul/matmul.cuh
@@ -7,12 +7,14 @@
 template<typename T>
 class MatMulWrapper {
 
-    using MatT = Eigen::Matrix<T, Eigen::Dynamic, Eigen::RowMajor>;
+	using MatT = Eigen::Matrix<T, Eigen::Dynamic, Eigen::RowMajor>;
 
-    MatMulWrapper(size_t aRows, size_t aCols): rows(aRows), cols(aCols), matrix(MatT::Random(aRows, aCols))
-    {   
-        CUDA_CHECK(cudaMalloc(&dMatrix, numBytes()));
-    }
+public:
+	MatMulWrapper(size_t aRows, size_t aCols) :
+		rows(aRows), cols(aCols), matrix(MatT::Random(aRows, aCols))
+	{
+		CUDA_CHECK(cudaMalloc(&dMatrix, numBytes()));
+	}
 
 	MatMulWrapper(const MatMulWrapper &)            = delete;
 	MatMulWrapper &operator=(const MatMulWrapper &) = delete;
@@ -21,12 +23,12 @@ class MatMulWrapper {
 
 	void upload() // Upload host matrix to device
 	{
-        CUDA_CHECK(cudaMemcpy(dMatrix, matrix.data(), numBytes(), cudaMemcpyHostToDevice));
+		CUDA_CHECK(cudaMemcpy(dMatrix, matrix.data(), numBytes(), cudaMemcpyHostToDevice));
 	}
 
 	void download() // Download device matrix to host
 	{
-        CUDA_CHECK(cudaMemcpy(matrix.data(), dMatrix, cudaMemcpyDeviceToHost));
+		CUDA_CHECK(cudaMemcpy(matrix.data(), dMatrix, cudaMemcpyDeviceToHost));
 	}
 
 	size_t numBytes()
@@ -34,23 +36,9 @@ class MatMulWrapper {
 		return rows * cols * sizeof(T);
 	}
 
-    float *matrixData()
-    {
-        if (!matrix)
-            throw std::runtime_error("MatMulWrapper::matrixData(): empty host matrix");
-        return matrix.data();
-    }
-    
-    float *deviceMatrixData()
-    {
-        if (!dMatrix)
-            throw std::runtime_error("MatMulWrapper::deviceMatrixData(): empty host matrix");
-        return dMatrix;
-    }
-
-private:
-    size_t rows{0};
-    size_t cols{0};
+public:
+	size_t rows{0};
+	size_t cols{0};
 	MatT matrix;
 	float *dMatrix{nullptr};
 };
