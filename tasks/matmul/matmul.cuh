6e9bd2d2d0af6d1cbee28f18a7f51a2db4fb9fa8
14Og <kolya.kiz@yandex.ru>
2026-06-26 11:30:01 +0000

    (impl) autosync: 11:30:01

--- tasks/matmul/matmul.cuh
+++ tasks/matmul/matmul.cuh
@@ -4,12 +4,18 @@
 #include "Eigen/Dense"
 
 template<typename T>
-struct MatMulWrapper {
+class MatMulWrapper {
 
-	Eigen::Matrix<T, Eigen::Dynamic, Eigen::RowMajor> matrix;
-	float *dMatrix{nullptr};
-	size_t rows{0};
-	size_t cols{0};
+    using MatT = Eigen::Matrix<T, Eigen::Dynamic, Eigen::RowMajor>;
+
+    MatMulWrapper(size_t aRows, size_t aCols): rows(aRows), cols(aCols), matrix(MatT::Random(aRows, aCols))
+    {   
+        
+    }
+	MatMulWrapper(const MatMulWrapper &)            = delete;
+	MatMulWrapper &operator=(const MatMulWrapper &) = delete;
+	MatMulWrapper(MatMulWrapper &&)                 = delete;
+	MatMulWrapper &operator=(MatMulWrapper &&)      = delete;
 
 	void upload()
 	{
@@ -23,6 +29,12 @@ struct MatMulWrapper {
 	{
 		return 0;
 	}
+
+private:
+    size_t rows{0};
+    size_t cols{0};
+	MatT matrix;
+	float *dMatrix{nullptr};
 };
 
 #endif /* TASKS_MATMUL_MATMUL_CUH_ */
