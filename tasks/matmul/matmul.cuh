8ad78b8a048b696a9bd27de23316520a768eee03
14Og <kolya.kiz@yandex.ru>
2026-06-26 11:21:30 +0000

    (impl) sync

--- tasks/matmul/matmul.cuh
+++ tasks/matmul/matmul.cuh
@@ -1,5 +1,29 @@
 #ifndef TASKS_MATMUL_MATMUL_CUH_
 #define TASKS_MATMUL_MATMUL_CUH_
 
+#include "Eigen/Dense"
+#include <Eigen/src/Core/util/Constants.h>
+
+template<typename T>
+struct MatMulWrapper {
+
+	Eigen::Matrix<T, Eigen::Dynamic, Eigen::RowMajor> matrix;
+	float *dMatrix{nullptr};
+	size_t rows{0};
+	size_t cols{0};
+
+	void upload()
+	{
+	}
+
+	void download()
+	{
+	}
+
+	size_t numBytes()
+	{
+		return 0;
+	}
+};
 
 #endif /* TASKS_MATMUL_MATMUL_CUH_ */
