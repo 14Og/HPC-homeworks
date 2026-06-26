c2eec475360b4b6564814433b83f42de599ab14d
14Og <kolya.kiz@yandex.ru>
2026-06-26 12:44:01 +0000

    (impl) cuda: added matmul wrapper for convenient download/upload and kernel invocation

--- tasks/matmul/matmul.cu
+++ tasks/matmul/matmul.cu
@@ -1,19 +1,24 @@
+
 #include "matmul.cuh"
 #include "matmul_kernels.cuh"
 
+#include <iostream>
+
 static constexpr dim3 kBlock(16, 16);
 
-int main()
+int main(int argc, char **argv)
 {
+	if (argc != 4) {
+		std::cerr << "usage: matmul_test <M> <N> <K>\n";
+		return EXIT_FAILURE;
+	}
+	const size_t m = std::stoul(argv[1]);
+	const size_t n = std::stoul(argv[2]);
+	const size_t k = std::stoul(argv[3]);
 
-    const size_t M = std::stoul(argv[1]);
-    const size_t N = std::stoul(argv[2]);
-    const size_t K = std::stoul(argv[3]);
-
-    
-	MatMulWrapper<float> A(100, 100, kRandomInit);
-	MatMulWrapper<float> B(100, 100, kRandomInit);
-	MatMulWrapper<float> C(100, 100, kEmptyInit);
+	MatMulWrapper<float> A(m, n, kRandomInit);
+	MatMulWrapper<float> B(n, k, kRandomInit);
+	MatMulWrapper<float> C(m, k, kEmptyInit);
 
 	auto Cref = A.matrix * B.matrix;
 
@@ -28,19 +33,18 @@ int main()
 
 	CUDA_CHECK(cudaEventRecord(start));
 
-	Kernels::matMulNaive<<<grid, kBlock>>>(
-		A.dMatrix, B.dMatrix, C.dMatrix, A.rows, A.cols, B.cols);
+	Kernels::matMulNaive<<<grid, kBlock>>>(A.dMatrix, B.dMatrix, C.dMatrix, A.rows, A.cols, B.cols);
 
 	CUDA_CHECK(cudaGetLastError());
 	CUDA_CHECK(cudaEventRecord(stop));
 	CUDA_CHECK(cudaEventSynchronize(stop));
-    
-    float ms = 0;
-    CUDA_CHECK(cudaEventElapsedTime(&ms, start, stop));
-    std::printf("Kernel time: %.3f\n", ms);
 
-    cudaEventDestroy(start);
-    cudaEventDestroy(stop);
+	float ms = 0;
+	CUDA_CHECK(cudaEventElapsedTime(&ms, start, stop));
+	std::printf("Kernel time: %.3f ms\n", ms);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 
 	C.download();
 }
